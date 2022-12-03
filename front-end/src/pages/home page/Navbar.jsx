25eb0d7581056899268b95c91fbeaf00e436815c
bipulkumar254 <bipulkumar2540@yahoo.com>
2022-12-04 02:57:43 +0530

    home

---
+++ front-end/src/pages/home page/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./../../style/Navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.container}>Navbar</div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ front-end/src/pages/home page/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./../../style/Sidebar.module.scss";
+
+const Sidebar = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.container}>Sidebar</div>
+    </div>
+  );
+};
+
+export default Sidebar;
